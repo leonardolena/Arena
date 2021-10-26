9e859da54b49e50ad402cb8d16356e9a559c7ffa
leonardolena <74065979+leonardolena@users.noreply.github.com>
2021-10-26 22:09:04 +0200

    initial modifications

---
+++ Arena/Fighters/Assassin.cs
@@ -0,0 +1,16 @@
+﻿namespace ArenaApplication
+{
+    class Assassin : Fighter
+    {
+        public Weapon Stiletto = new(45);
+        public int Damage => Stiletto.GetDamage()*Strength/100;
+
+        public override int Attack() {
+            return (int)GetModifier() % Damage;
+        }
+
+        public double GetModifier() {
+            return Health;
+        }
+    }
+}
